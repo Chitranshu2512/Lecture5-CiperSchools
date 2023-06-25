287d23c56be8e6fdb17d3fd01d70a9f083af332a
Chitranshu2512 <118387802+Chitranshu2512@users.noreply.github.com>
2023-06-25 23:34:58 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,21 @@
+public class Operator {
+    public static void main(String[] args) {
+
+        int x=5;
+        int y=25;
+
+        int z = x+y ;
+//        + -> operator
+//      x,y -> operands
+
+//        increment and decrement operator
+
+        int m = 5 ;
+        int n = m++ ;
+        System.out.println("m is "+m+", n is "+n);
+
+        int p = 5 ;
+        int q = ++p ;
+        System.out.println("p is "+p+ ", q is "+q);
+    }
+}
